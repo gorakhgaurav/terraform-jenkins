dbd8b735845244b9c504a3d75d2c99e8c594406e
gorakhgaurav <139329569+gorakhgaurav@users.noreply.github.com>
2024-04-08 15:21:24 +0530

    Update minikube.tf

--- terraform-minikube/minikube.tf
+++ terraform-minikube/minikube.tf
@@ -1,85 +1,83 @@
-#!/bin/bash
+provider "aws" {
+  region  = var.region
+}
 
-#  Set hostname on Each Node
+resource "aws_security_group" "MinikubeSG" {
+  name = "Minikube SG"
 
-sudo hostnamectl set-hostname "k8smaster.example.net"
-exec bash
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-IP_ADDRESS=$(hostname -I | awk '{print $1}')
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-DOMAIN_NAME="k8smaster.example.net k8smaster"
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-echo "$IP_ADDRESS    $DOMAIN_NAME" | sudo tee -a /etc/hosts
+  ingress {
+    from_port   = 8443
+    to_port     = 8443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-# Disable swap & Add kernel Parameters
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-sudo swapoff -a
-sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
-sudo tee /etc/modules-load.d/containerd.conf <<EOF
-overlay
-br_netfilter
-EOF
-sudo modprobe overlay
-sudo modprobe br_netfilter
+resource "aws_instance" "MinikubeEC2" {
+  instance_type          = var.instance_type
+  ami                    = data.aws_ami.ubuntu.id
+  vpc_security_group_ids = [aws_security_group.MinikubeSG.id]
+  key_name               = var.ssh_key_name
 
-sudo tee /etc/sysctl.d/kubernetes.conf <<EOT
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-net.ipv4.ip_forward = 1
-EOT
+  tags = {
+    Name        = "Minikube-Instance"
+    Environment = "Dev"
+    Project     = "Promotion Assessment"
+  }
+#  user_data = file("userdata.sh")
+}
 
-sudo sysctl --system
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-# CRI-O install
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-22.04-amd64-server-*"]
+  }
 
-sudo apt update -y
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 
-sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
+  owners = ["099720109477"]  # Canonical
+}
 
-export OS=xUbuntu_22.04
-export CRIO_VERSION=1.24
-
-echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /"| sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
-echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$CRIO_VERSION/$OS/ /"|sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$CRIO_VERSION.list
-
-curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$CRIO_VERSION/$OS/Release.key | sudo apt-key add -
-
-curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | sudo apt-key add -
-
-sudo apt update -y
-
-sudo apt install cri-o cri-o-runc -y
-
-sudo systemctl start crio
-sudo systemctl enable crio
-
-sudo apt install containernetworking-plugins -y
-
-sudo systemctl restart crio
-
-sudo apt install -y cri-tools
-
-sudo crictl --runtime-endpoint unix:///var/run/crio/crio.sock version
-
-# Add Apt Repository for Kubernetes
-
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-sudo apt update
-sudo apt install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
-
-sudo kubeadm init --control-plane-endpoint=k8smaster.example.net
-
-sleep 20s
-
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-kubectl cluster-info
-
-kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+output "minikube_endpoint" {
+  value = formatlist("http://%s:%s/", aws_instance.MinikubeEC2.*.public_ip, "8080")
+}
