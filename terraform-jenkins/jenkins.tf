47e3ef5f886d3b6074ad2a11a478f472143b4803
gorakhgaurav <139329569+gorakhgaurav@users.noreply.github.com>
2024-04-08 15:23:44 +0530

    Update jenkins.tf

--- terraform-jenkins/jenkins.tf
+++ terraform-jenkins/jenkins.tf
@@ -19,6 +19,13 @@ resource "aws_security_group" "JenkinsSG" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
